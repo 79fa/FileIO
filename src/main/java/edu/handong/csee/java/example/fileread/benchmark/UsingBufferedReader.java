69026a4bc0c5cff31b2f7ad51b4d24e8a653c17b
79fa <82189633+79fa@users.noreply.github.com>
2021-05-30 21:02:20 +0900

    Update UsingBufferedReader.java

--- src/main/java/edu/handong/csee/java/example/fileread/benchmark/UsingBufferedReader.java
+++ src/main/java/edu/handong/csee/java/example/fileread/benchmark/UsingBufferedReader.java
@@ -34,14 +34,19 @@ public class UsingBufferedReader {
         //-------------- Test reading 10 MB file. --------------------
 
         StopWatch.start();
+        try{
+            
+            BufferedReader inputStream2= new BufferedReader(new FileReader(args[1]));
+            while (inputStream2.read()!=-1){}
 
-        BufferedReader inputStream2= new BufferedReader(new FileReader(DumpDataWriter.input10MB));
-        while (inputStream2.read()!=-1){}
+            long duration2 = StopWatch.stop();
+            System.out.println(duration2 + " milsec");
 
-        long duration2 = StopWatch.stop();
-        System.out.println(duration2 + " milsec");
-
-        inputStream2.close();
+            inputStream2.close();
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+            Sysytem.out.println("error");
+        }
 
         /*
         //-------------- Test reading 100 MB file. --------------------
